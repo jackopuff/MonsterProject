3d1c2bbe0204d9329f4e7099f1d29d9ebe80cb56
jackopuff <jcar8281@740s-pl-128.local>
2016-09-14 13:34:39 -0600

    added getters

--- src/monster/controller/MonsterController.java
+++ src/monster/controller/MonsterController.java
@@ -6,12 +6,19 @@ public class MonsterController
 	
 	public MonsterController()
 	{
-		firstMonster = new MarshmallowMonster("herman", 1.7,1,1,2,1,true, true);
+		firstMonster = new MarshmallowMonster("herman",1,1.7,1,2,1,true,true);
 	}
 	public void start ()
 	{
 		System.out.println("We made monsters today!");
 		System.out.println("Here is mine " + firstMonster);
+		System.out.println("He has " +firstMonster.getLegCount() + " legs.");
+		System.out.println("He has " +firstMonster.getArmCount() + " arms.");
+		System.out.println("He has " +firstMonster.getNoseCount() + " nose.");
+		System.out.println("He has " +firstMonster.getEyeCount() + " eye.");
+		System.out.println("He has " +firstMonster.getAntennaCount() + " antennae.");
+		System.out.println("It is " +firstMonster.getHasHair() + " that he has hair.");
+		System.out.println("It is " +firstMonster.getHasBellyButton() + " that he has a belly button.");
 	}
 	/*monster name is herman
 	 * has 1 antenna
--- src/monster/model/MarshmallowMonster.java
+++ src/monster/model/MarshmallowMonster.java
@@ -20,15 +20,62 @@ public class MarshmallowMonster
 		this.noseCount = -2;
 		this.hasBellyButton = false;
 	}
-	public MarshmallowMonster(String name, double armCount, int eyeCount, int antennaCount, int legCount, int noseCount, boolean hasBellyButton, boolean hasHair)
+	public MarshmallowMonster(String name, int eyeCount, double armCount, int antennaCount, int legCount, int noseCount, boolean hasBellyButton, boolean hasHair)
 	{
 		this.name = name;
 		this.armCount = armCount;
+		this.eyeCount = eyeCount;
 		this.antennaCount = antennaCount;
 		this.legCount = legCount;
 		this.noseCount = noseCount;
 		this.hasBellyButton = hasBellyButton;
 		this.hasHair = hasHair;		
+	}
+	
+	public String toString()
+	{
+		String description = "Monster says: Name is " + name;
+		
+		return description;
+	}
+	
+	public String getName()
+	{
+		return name;
+	}
+	public int getEyeCount()
+	{
+		return eyeCount;
+	}
+	
+	public boolean getHasBellyButton()
+	{
+		return hasBellyButton;
+	}
+	
+	public int getLegCount()
+	{
+		return legCount; 
+	}
+	
+	public boolean getHasHair()
+	{
+		return hasHair;
+	}
+	
+	public double getArmCount()
+	{
+		return armCount;
+	}
+	
+	public int getNoseCount()
+	{
+		return noseCount;
+	}
+	
+	public int getAntennaCount()
+	{
+		return antennaCount;
 	}
 /*monster name is herman
  * has 1 antenna
