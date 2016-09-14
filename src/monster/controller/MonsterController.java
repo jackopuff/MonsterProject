98824127e2bf5e0fbb6241b08010325ffa1a648a
jackopuff <jcar8281@740s-pl-128.local>
2016-09-14 12:56:15 -0600

    made a monster, defined parts of monster

--- src/monster/controller/MonsterController.java
+++ src/monster/controller/MonsterController.java
@@ -1,9 +1,25 @@
 package monster.controller;
-
-public class MonsterController {
+import monster.model.MarshmallowMonster;
+public class MonsterController 
+{
+	private MarshmallowMonster firstMonster;
+	
+	public MonsterController()
+	{
+		firstMonster = new MarshmallowMonster("herman", 1.7,1,1,2,1,true, true);
+	}
 	public void start ()
 	{
-		
+		System.out.println("We made monsters today!");
+		System.out.println("Here is mine " + firstMonster);
 	}
-
+	/*monster name is herman
+	 * has 1 antenna
+	 * has2 legs
+	 * has 2 arms
+	 * has 1 nose
+	 * has 1 eye
+	 * has belly button
+	 * has hair
+	 */
 }
