d072983600358b36cc1026e5fb8b84bdb6b6adb8
jackopuff <jcar8281@740s-pl-128.local>
2016-09-16 13:05:34 -0600

    added the ability to eat one of the monster's arms or legs with setters, if then's, and getters.

--- src/monster/controller/MonsterController.java
+++ src/monster/controller/MonsterController.java
@@ -21,6 +21,8 @@ public class MonsterController
 		System.out.println("He has " +firstMonster.getAntennaCount() + " antennae.");
 		System.out.println("It is " +firstMonster.getHasHair() + " that he has hair.");
 		System.out.println("It is " +firstMonster.getHasBellyButton() + " that he has a belly button.");
+		
+		
 		System.out.println("would you like to change my name??");
 		String answer =keyboardInput.nextLine();
 		if(answer.equalsIgnoreCase("yes"))
@@ -34,6 +36,35 @@ public class MonsterController
 			System.out.println("I get to keep my name grrr I don\'t like it angry emoji >:(");
 		}
 		System.out.println(firstMonster);
+		
+		
+		System.out.println("do you want to eat my eye?");
+		String eyeAnswer=keyboardInput.nextLine();
+		if(eyeAnswer.equalsIgnoreCase("yes"))
+		{
+			System.out.println("aaaaah!");
+			firstMonster.setEyeCount(0);
+		}
+		else
+		{
+			System.out.println("phew thats a relief");
+		}
+		System.out.println("my current number of eyes is:");
+		System.out.println(firstMonster.getEyeCount());
+		
+		
+		System.out.println("would you like to eat one of my legs?");
+		String legAnswer=keyboardInput.nextLine();
+		if (legAnswer.equalsIgnoreCase("yes"))
+		{
+			System.out.println("oh no!");
+			firstMonster.setLegCount(firstMonster.getLegCount()-1);
+		}
+		else
+		{
+			System.out.println("good I like my legs.");			
+		}
+		System.out.println("I have " + firstMonster.getLegCount() +" legs.");
 	}
 	/*monster name is herman
 	 * has 1 antenna
